009fc0b7bf77e5167a5227e038705dc1c3e167b2
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2016-03-06 03:58:54 +0900

    Add Okhttp

--- app/src/main/java/jp/ac/dendai/im/cps/citywalkersmeter/LogData.java
+++ app/src/main/java/jp/ac/dendai/im/cps/citywalkersmeter/LogData.java
@@ -1,67 +1,53 @@
 package jp.ac.dendai.im.cps.citywalkersmeter;
 
-/**
- * Created by naoya on 2015/08/19.
- */
-public class LogData {
-    private double latitude;
-    private double longitude;
-    private float accuracy;
-    private int time;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class LogData implements Serializable {
+    private double lat;
+    private double lng;
+    private double accuracy;
+    private double altitude;
+    private double accuracy_altitude;
     private float speed;
-    private  double altitude;
+    private float light;
+    private float pressure;
+    private float temprature;
+    private float humidity;
     private float bearing;
+    private float[] accelerometers;
+    private float[] gyroscope;
+    private float sound;
+    private float step;
+    private int is_active;
+    private String myway;
+    private String description;
+    private int timestamp;
 
-    public static LogData newInstance(double latitude, double longitude, float accuracy, int time, float speed, double altitude, float bearing) {
-        LogData data = new LogData();
-        data.setLatitude(latitude);
-        data.setLongitude(longitude);
-        data.setAccuracy(accuracy);
-        data.setTime(time);
-        data.setSpeed(speed);
-        data.setAltitude(altitude);
-        data.setBearing(bearing);
-        return data;
+    public double getLat() {
+        return lat;
     }
 
-    public double getLatitude() {
-        return latitude;
+    public void setLat(double lat) {
+        this.lat = lat;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
+    public double getLng() {
+        return lng;
     }
 
-    public double getLongitude() {
-        return longitude;
+    public void setLng(double lng) {
+        this.lng = lng;
     }
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public float getAccuracy() {
+    public double getAccuracy() {
         return accuracy;
     }
 
-    public void setAccuracy(float accuracy) {
+    public void setAccuracy(double accuracy) {
         this.accuracy = accuracy;
-    }
-
-    public double getTime() {
-        return time;
-    }
-
-    public void setTime(int time) {
-        this.time = time;
-    }
-
-    public float getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
     }
 
     public double getAltitude() {
@@ -72,6 +58,54 @@ public class LogData {
         this.altitude = altitude;
     }
 
+    public double getAccuracy_altitude() {
+        return accuracy_altitude;
+    }
+
+    public void setAccuracy_altitude(double accuracy_altitude) {
+        this.accuracy_altitude = accuracy_altitude;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getLight() {
+        return light;
+    }
+
+    public void setLight(float light) {
+        this.light = light;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+
+    public float getTemprature() {
+        return temprature;
+    }
+
+    public void setTemprature(float temprature) {
+        this.temprature = temprature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
     public float getBearing() {
         return bearing;
     }
@@ -80,14 +114,78 @@ public class LogData {
         this.bearing = bearing;
     }
 
-    public String JSONFormat() {
-        return "{ \"lat\": " + this.latitude + ","
-                + " \"lng\":" + this.longitude + ","
-                + " \"accuracy\":" + (float)this.accuracy + ","
-                + " \"timestamp\":" + this.time + ","
-                + " \"speed\":" + this.speed + ","
-                + " \"altitude\":" + this.altitude + ","
-                + " \"bearing\":" + this.bearing
-                + "}";
+    public float[] getAccelerometers() {
+        return accelerometers;
     }
+
+    public void setAccelerometers(float[] accelerometers) {
+        this.accelerometers = accelerometers;
+    }
+
+    public float[] getGyroscope() {
+        return gyroscope;
+    }
+
+    public void setGyroscope(float[] gyroscope) {
+        this.gyroscope = gyroscope;
+    }
+
+    public float getSound() {
+        return sound;
+    }
+
+    public void setSound(float sound) {
+        this.sound = sound;
+    }
+
+    public float getStep() {
+        return step;
+    }
+
+    public void setStep(float step) {
+        this.step = step;
+    }
+
+    public int getIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(int is_active) {
+        this.is_active = is_active;
+    }
+
+    public String getMyway() {
+        return myway;
+    }
+
+    public void setMyway(String myway) {
+        this.myway = myway;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+//    public String JSONFormat() {
+//        return "{ \"lat\": " + this.latitude + ","
+//                + " \"lng\":" + this.longitude + ","
+//                + " \"accuracy\":" + (float)this.accuracy + ","
+//                + " \"timestamp\":" + this.time + ","
+//                + " \"speed\":" + this.speed + ","
+//                + " \"altitude\":" + this.altitude + ","
+//                + " \"bearing\":" + this.bearing
+//                + "}";
+//    }
 }
