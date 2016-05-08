d1d475641e47beed96e8ee643465755114800bf9
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2016-05-08 16:11:28 +0900

    Update UrlBuilder

--- app/src/main/java/jp/ac/dendai/im/cps/citywalkersmeter/networks/UrlBuilder.java
+++ app/src/main/java/jp/ac/dendai/im/cps/citywalkersmeter/networks/UrlBuilder.java
@@ -1,17 +1,18 @@
 package jp.ac.dendai.im.cps.citywalkersmeter.networks;
 
+import jp.ac.dendai.im.cps.citywalkersmeter.BuildConfig;
 import okhttp3.HttpUrl;
 
 public class UrlBuilder {
     private static final String TAG = UrlBuilder.class.getSimpleName();
 
-    public static final String DON_SCHEME = "http";
-    public static final String DON_HOST = "citylog.cps.im.dendai.ac.jp";
-    public static final String ROOT = "api";
-    public static final String USERS = "users";
-    public static final String REGIST = "regist";
-    public static final String LOGS = "logs";
-    public static final String UPDATE = "update";
+    public static final String SCHEME = BuildConfig.CITYLOG_SCHEME;
+    public static final String HOST = BuildConfig.CITYLOG_HOST;
+    public static final String ROOT = BuildConfig.CITYLOG_ROOT;
+    public static final String USERS = BuildConfig.CITYLOG_USERS;
+    public static final String REGIST = BuildConfig.CITYLOG_REGIST;
+    public static final String LOGS = BuildConfig.CITYLOG_LOGS;
+    public static final String UPDATE = BuildConfig.CITYLOG_UPDATE;
 
     /**
      * /api/
@@ -19,8 +20,8 @@ public class UrlBuilder {
      */
     private static HttpUrl.Builder buildRootUrl() {
         return new HttpUrl.Builder()
-                .scheme(DON_SCHEME)
-                .host(DON_HOST)
+                .scheme(SCHEME)
+                .host(HOST)
                 .addPathSegment(ROOT);
     }
 
